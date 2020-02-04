5d01a1fcc76b44d65d7f43d483aa92dbe2104c73
Adam Single <adam.single@unity3d.com>
2020-02-04 13:42:46 +1000

    ADD: ARSessionStarter and view ADD: Main scene with starter and view

---
+++ Assets/Scripts/ARSessionStarter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace Assets.Scripts
+{
+    public class ARSessionStarter : MonoBehaviour
+    {
+        private ARSessionStarterView _view;
+        private ARSession _session;
+
+        private void Awake()
+        {
+            _view = FindObjectOfType<ARSessionStarterView>();
+            if (_view == null)
+            {
+                Debug.LogWarning("No ARSessionMonitorView was found in the scene");
+            }
+            _session = FindObjectOfType<ARSession>();
+            if (_session == null)
+            {
+                Debug.LogWarning("There is no ARSession in the scene for the ARSessionMonitor to monitor");
+            }
+            else
+            {
+                ARSession.stateChanged += ARSession_stateChanged;
+                StartCoroutine(StartSession());
+            }
+        }
+
+        private void ARSession_stateChanged(ARSessionStateChangedEventArgs obj)
+        {
+            if(_view != null)
+            {
+                _view.SessionState = ARSession.state.ToString();
+            }
+        }
+
+        private IEnumerator StartSession()
+        {
+            if ((ARSession.state == ARSessionState.None 
+                || ARSession.state == ARSessionState.CheckingAvailability))
+            {
+                yield return ARSession.CheckAvailability();
+            }
+
+            if (ARSession.state == ARSessionState.Unsupported)
+            {
+                Debug.LogWarning("Device does not support AR");
+            }
+            else
+            {
+                // Start the AR session
+                _session.enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ARSessionStarterView.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class ARSessionStarterView : MonoBehaviour , INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string _sessionState;
+
+    [Binding]
+    public string SessionState
+    {
+        get => _sessionState;
+        set
+        {
+            if (value == _sessionState)
+            {
+                return;
+            }
+
+            _sessionState = value;
+            OnPropertyChanged(nameof(SessionState));
+        }
+    }
+}
